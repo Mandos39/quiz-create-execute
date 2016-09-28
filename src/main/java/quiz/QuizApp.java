2ff0d8625eebf147b34b9faaf9c1bf77f650b304
Mandos39 <stefano.allegretti@hotmail.it>
2016-09-28 23:43:28 +0200

    commit message

--- src/main/java/quiz/QuizApp.java
+++ src/main/java/quiz/QuizApp.java
@@ -11,10 +11,10 @@ public class QuizApp extends JFrame implements ActionListener {
 
     public QuizApp() {
         super();
-        add(new FirstPanel("Frase di rito", this));
-        //add(new ProvaForm(this).getPanel1());
+        //add(new FirstPanel("Frase di rito", this));
+        add(new ProvaForm(this).getPanel1());
         //add(new quiz.QuestionPanel(3, "Quanti anni hai?", this));
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
         pack();
     }
@@ -26,8 +26,8 @@ public class QuizApp extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if ("GoToQuestion".equals(e.getActionCommand())) {
             getContentPane().removeAll();
-            add(new QuestionPanel(3, "Quanti anni hai?", this));
-            //add(new RitualForm().getPagnello());
+            //add(new QuestionPanel(3, "Quanti anni hai?", this));
+            add(new RitualForm().getPagnello());
             pack();
         }
 
